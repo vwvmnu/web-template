0c78e2a4115a5b8ff6396e7cad9b6071dfc24d30
blackbox <vwvmnu@gmail.com>
2023-09-17 16:44:00 +0800

    feat(template): Implement carouselList with associated assets
    - Added carouselList.html for structured carousel display.
    - Introduced related JS and CSS for functionality and styling.
    - Completed the scrolling list feature for enhanced user experience.

---
+++ template/bootstrap5+vue3/module/carouselList.js
@@ -0,0 +1,33 @@
+let count = 0;
+function addSlide() {
+    // Create a new slide
+    let newSlide = document.createElement('div');
+    newSlide.className = 'slide';
+    newSlide.innerText = 'New Content' + count;
+    count++;
+
+    // Append the new slide to the container
+    let container = document.querySelector('.container');
+    container.appendChild(newSlide);
+
+    // Move the slides upwards
+    const timeoutA = setTimeout(()=>{
+        let slides = document.querySelectorAll('.slide');
+        for (let slide of slides) {
+            slide.style.transition = 'transform 0.4s ease-in-out';
+            slide.style.transform = `translateY(-50px)`;
+        }
+    }, 100)
+
+
+    // Remove the first slide after the transition
+    const timeoutB = setTimeout(() => {
+        let slides = document.querySelectorAll('.slide');
+        container.removeChild(slides[0]);
+        // Reset transform for remaining slides
+        for (let slide of document.querySelectorAll('.slide')) {
+            slide.style.transition = 'none'
+            slide.style.transform = `translateY(0px)`;
+        }
+    }, 500);
+}
