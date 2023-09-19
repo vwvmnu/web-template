5b54e5ca5405c1285ce6fd5bffbc9ad8dad520a2
blackbox <vwvmnu@gmail.com>
2023-09-19 09:45:30 +0800

    refactor(web4 & web2): Enhance logic, text, and layout
    - Optimized the underlying logic in web4 for better performance and clarity.
    - Improved textual content in web4 for enhanced readability.
    - Refined the page layout in web2 for a more streamlined user experience.

--- web4-画册试用页面/js/index.js
+++ web4-画册试用页面/js/index.js
@@ -4,6 +4,7 @@ const displayAd = () => {
     canvasBottom.show()
 }
 const pushData = () => {
+    displayAd2();
     $.ajax({
         async: true,
     })
@@ -13,4 +14,27 @@ const canvasMainUpload = () => {
     let input = document.createElement('input');
     input.type = 'file';
     input.click();
+}
+// const canvasBottom1 = new bootstrap.Offcanvas('#canvasBottom1')
+// const canvasBottom2 = new bootstrap.Offcanvas('#canvasBottom2')
+// const canvasBottom3 = new bootstrap.Offcanvas('#canvasBottom3')
+
+const displayAd1 = () => {
+    console.log("hhhh1")
+    const canvasBottom1 = new bootstrap.Offcanvas('#canvasBottom1')
+    canvasBottom1.show()
+}
+const displayAd2 = () => {
+    console.log("hhhh2")
+    const canvasBottom1 = new bootstrap.Offcanvas('#canvasBottom1')
+    const canvasBottom2 = new bootstrap.Offcanvas('#canvasBottom2')
+    canvasBottom1.hide()
+    canvasBottom2.show()
+}
+const displayAd3 = () => {
+    console.log("hhhh3")
+    const canvasBottom2 = new bootstrap.Offcanvas('#canvasBottom2')
+    const canvasBottom3 = new bootstrap.Offcanvas('#canvasBottom3')
+    canvasBottom2.hide()
+    canvasBottom3.show()
 }
