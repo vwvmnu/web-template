a701ffab1ff355468baf68bad12853ab44d923ab
blackbox <vwvmnu@gmail.com>
2023-09-21 11:46:30 +0800

    refactor(template): Enhance structure and compatibility of carousel table
    - Optimized the structural design of the carousel table for better clarity and organization.
    - Improved compatibility to ensure consistent display across various devices and browsers.

--- template/module/dubble/bubble.js
+++ template/module/dubble/bubble.js
@@ -1,24 +1,30 @@
 let count = 0;
 
-const msgList = ["小白小在xxxx前提交了xxxxxx", "456在xxxx前提交了xxxxxx", "789在xxxx前提交了xxxxxx", "-1235在xxxx前提交了xxxxxx", "-4563在xxxx前提交了xxxxxx", "我的你的在xxxx前提交了xxxxxx"]
-const size = 3;
-const runningMark = true;
+const msgList = ["小白小在8秒前提交了ai画册", "456在3秒前开启了ai画册", "小黑在8秒前提交了ai画册", "小白小在8秒前提交了ai画册", "小白小在8秒前提交了ai画册", "小白小在8秒前提交了ai画册", "123"]
+const size = 5;
+let runningMark = true;
+let height = 50;
 
 /**
  * 休眠函数
  * @param ms
  * @returns {Promise<unknown>}
  */
-const sleep = (ms) => {
+const mySleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const initCarouserList = (msgList = [""], size = 3) => {
+/**
+ * 初始化列表
+ * @param msgList 消息列表
+ * @param size 初始化大小
+ * @param container 父级document元素
+ */
+const initCarouserList = (msgList = [""], size = 3, container = document.querySelector('.carouselList-container')) => {
     // 绘制列表
-    let container = document.querySelector('.carouselList-container');
     for (let i = 0; i < size; i++) {
         let newSlide = document.createElement('div');
-        newSlide.className = 'slide';
+        newSlide.className = 'mySlide';
         newSlide.innerText = msgList[msgList.length - i - 1];
         // debugger
         container.appendChild(newSlide);
@@ -26,6 +32,7 @@ const initCarouserList = (msgList = [""], size = 3) => {
 }
 
 const startCarouserList = async (timeSize = 0) => {
+    runningMark = true;
     while (runningMark) {
         for (let i = 0; i < msgList.length; i++) {
             await addSlide(msgList[i]);
@@ -33,45 +40,53 @@ const startCarouserList = async (timeSize = 0) => {
     }
 }
 
+const StopCarouserList = () =>{
+    runningMark = false;
+}
 
-async function addSlide(msg) {
+/**
+ * 添加字元素
+ * @param msg 消息
+ * @param container
+ * @returns {Promise<void>}
+ */
+async function addSlide(msg, container = document.querySelector('.carouselList-container')) {
     // Create a new slide
     let newSlide = document.createElement('div');
-    newSlide.className = 'slide';
+    newSlide.className = 'mySlide';
     newSlide.innerText = msg;
     count++;
 
     // Initially place the new slide below the visible area based on container's height
 
     // Append the new slide to the container
-    let container = document.querySelector('.carouselList-container');
     container.appendChild(newSlide);
 
-    await sleep(32);  // Small delay to ensure the new slide's transform is applied
+    await mySleep(32);  // Small delay to ensure the new slide's transform is applied
 
     // Move the slides upwards
-    let slides = document.querySelectorAll('.slide');
+    let slides = document.querySelectorAll('.mySlide');
     for (let slide of slides) {
         slide.style.transition = 'transform 0.4s ease-in-out';
-        slide.style.transform = `translateY(-50px)`;
+        slide.style.transform = `translateY(-${height}px)`;
     }
 
-    await sleep(400);  // Match the duration of the transition
+    await mySleep(400);  // Match the duration of the transition
     container.removeChild(slides[0]);
 
     // Reset transform for remaining slides
-    slides = document.querySelectorAll('.slide');
+    slides = document.querySelectorAll('.mySlide');
     for (let slide of slides) {
         slide.style.transition = 'none';
         slide.style.transform = `translateY(0px)`;
     }
 
-    await sleep(600);  // Remaining time to reach the total sleep time of 1000ms as earlier
+    await mySleep(600);  // Remaining time to reach the total sleep time of 1000ms as earlier
 }
 
 
 
 
-initCarouserList(msgList, 5);
+initCarouserList(msgList, size);
 startCarouserList().then();
 
