d61f6ed195fa7d892661c8a8802ad32b0da081be
blackbox <vwvmnu@gmail.com>
2023-09-25 16:15:05 +0800

    refactor(template/dubble): Update carousel table for dynamic row loading and customization
    - Modified the carousel table in the dubble folder under the template module.
    - Enabled dynamic loading of carousel rows.
    - Added support for customizing the size and position of the carousel.

--- template/module/dubble/bubble.js
+++ template/module/dubble/bubble.js
@@ -1,77 +1,135 @@
 let count = 0;
 
-const msgList = ["小白小在xxxx前提交了xxxxxx", "456在xxxx前提交了xxxxxx", "789在xxxx前提交了xxxxxx", "-1235在xxxx前提交了xxxxxx", "-4563在xxxx前提交了xxxxxx", "我的你的在xxxx前提交了xxxxxx"]
-const size = 3;
-const runningMark = true;
+const size = 5;
+let runningMark = true;
+let height = 25;
+
+const initMyBarrageConfig = {
+    display: "absolute",
+    zIndex: 1,
+    width: "100%",
+    position: "fixed",
+    bottom: "18%",
+    left: "2%",
+    textAlign: "left",
+    overflow: "hidden",
+    height: "50px",
+    // 显示几行
+    numberOfColumns: 3,
+    // 每个显示的高度大小
+    barrageHeight: 20,
+}
 
 /**
  * 休眠函数
  * @param ms
  * @returns {Promise<unknown>}
  */
-const sleep = (ms) => {
+const mySleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const initCarouserList = (msgList = [""], size = 3) => {
-    // 绘制列表
-    let container = document.querySelector('.carouselList-container');
-    for (let i = 0; i < size; i++) {
-        let newSlide = document.createElement('div');
-        newSlide.className = 'slide';
-        newSlide.innerText = msgList[msgList.length - i - 1];
-        // debugger
-        container.appendChild(newSlide);
-    }
-}
-
-const startCarouserList = async (timeSize = 0) => {
-    while (runningMark) {
-        for (let i = 0; i < msgList.length; i++) {
-            await addSlide(msgList[i]);
-        }
-    }
-}
-
-
-async function addSlide(msg) {
+/**
+ * 添加字元素
+ * @param msg 消息
+ * @param container
+ * @returns {Promise<void>}
+ */
+async function addSlide(msg, container = document.querySelector('.carouselList-container')) {
     // Create a new slide
     let newSlide = document.createElement('div');
-    newSlide.className = 'slide';
+    newSlide.className = 'mySlide';
     newSlide.innerText = msg;
     count++;
 
     // Initially place the new slide below the visible area based on container's height
 
     // Append the new slide to the container
-    let container = document.querySelector('.carouselList-container');
     container.appendChild(newSlide);
 
-    await sleep(32);  // Small delay to ensure the new slide's transform is applied
+    await mySleep(32);  // Small delay to ensure the new slide's transform is applied
 
     // Move the slides upwards
-    let slides = document.querySelectorAll('.slide');
+    let slides = container.querySelectorAll('.mySlide');
     for (let slide of slides) {
-        slide.style.transition = 'transform 0.4s ease-in-out';
-        slide.style.transform = `translateY(-50px)`;
+        let computedStyle = window.getComputedStyle(slide);
+        let marginTop = parseInt(computedStyle.marginTop, 10);
+        let height = parseInt(computedStyle.height, 10);
+        slide.style.transition = 'transform 0.8s ease-in-out';
+        slide.style.transform = `translateY(-${marginTop + height}px)`;
     }
 
-    await sleep(400);  // Match the duration of the transition
+
+    await mySleep(811);  // Match the duration of the transition
     container.removeChild(slides[0]);
 
     // Reset transform for remaining slides
-    slides = document.querySelectorAll('.slide');
+    slides = document.querySelectorAll('.mySlide');
     for (let slide of slides) {
         slide.style.transition = 'none';
         slide.style.transform = `translateY(0px)`;
     }
 
-    await sleep(600);  // Remaining time to reach the total sleep time of 1000ms as earlier
+    await mySleep(700);  // Remaining time to reach the total sleep time of 1000ms as earlier
+    // debugger
+}
+
+/**
+ * 初始化列表以及布局
+ * @param msgList 消息列表 msgList = ["123", "246", "789"]
+ * @param size 初始化大小
+ * @param myBarrage 父级document元素
+ * @param myBarrageConfig 配置
+ */
+const initCarouserList = (msgList = ["123", "246", "789"], myBarrage = document.querySelector('.myBarrage'), myBarrageConfig= {}) => {
+
+    // 弹幕的相关配置
+
+    myBarrageConfig = Object.assign(initMyBarrageConfig, myBarrageConfig);
+
+    myBarrageConfig.height = myBarrageConfig.numberOfColumns * myBarrageConfig.barrageHeight + "px"
+    myBarrage.style.setProperty('--height', myBarrageConfig.height);
+    myBarrage.style.setProperty('--numberOfColumns', myBarrageConfig.numberOfColumns);
+
+    myBarrage.style.display = myBarrageConfig.display;
+    myBarrage.style.zIndex = myBarrageConfig.zIndex;
+    myBarrage.style.height = myBarrageConfig.height;
+    myBarrage.style.width = myBarrageConfig.width;
+    myBarrage.style.position = myBarrageConfig.position;
+    myBarrage.style.bottom = myBarrageConfig.bottom;
+    myBarrage.style.left = myBarrageConfig.left;
+    myBarrage.style.textAlign = myBarrageConfig.textAlign;
+    myBarrage.style.overflow = myBarrageConfig.overflow;
+
+
+    // 绘制列表
+    for (let i = 0; i < myBarrageConfig.numberOfColumns; i++) {
+        let newSlide = document.createElement('div');
+        newSlide.className = 'mySlide';
+        newSlide.innerText = msgList[msgList.length - i - 1];
+        // debugger
+        myBarrage.appendChild(newSlide);
+    }
+}
+
+/**
+ * 启动滚动列表
+ * @param msgList 数据
+ * @param container 组件
+ * @param myBarrageConfig 配置 { numberOfColumns: 3, barrageHeight: 20,}
+ * @returns {Promise<void>}
+ */
+const startCarouserList = async (msgList = ["123", "246", "789"], container = document.querySelector('.carouselList-container'), myBarrageConfig={}) => {
+    initCarouserList(msgList,  container, myBarrageConfig);
+    runningMark = true;
+    while (runningMark) {
+        for (let i = 0; i < msgList.length; i++) {
+            await addSlide(msgList[i], container);
+        }
+    }
 }
 
 
-
-
-initCarouserList(msgList, 5);
-startCarouserList().then();
-
+const StopCarouserList = () => {
+    runningMark = false;
+}
